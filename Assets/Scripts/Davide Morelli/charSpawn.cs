3450598b3f4d03890844a68505df0c657594feee
daviidemorelli <davidemorelli@studenti.naba.it>
2021-01-30 16:31:22 +0100

    Aggiustato

--- Assets/Scripts/Davide Morelli/charSpawn.cs
+++ Assets/Scripts/Davide Morelli/charSpawn.cs
@@ -10,30 +10,31 @@ public class charSpawn : MonoBehaviour
     public Vector3 spawn3;
     public Vector3 spawn4;
     public randNumber randNumber;
-    private int randomSpawn;
+    public int randomSpawn;
+    public dogSpawnScript dogSpawnScript;
 
     // Start is called before the first frame update
     void Start()
     {
         randNumber.randNumberGen();
         randomSpawn = randNumber.numberRandom;
-        if (randomSpawn == 1)
+        if (randNumber.numberRandom == 1)
         {
             Instantiate(Character, spawn1, transform.rotation);
         }
-        if (randomSpawn == 2)
+        if (randNumber.numberRandom == 2)
         {
             Instantiate(Character, spawn2, transform.rotation);
         }
-        if (randomSpawn == 3)
+        if (randNumber.numberRandom == 3)
         {
             Instantiate(Character, spawn3, transform.rotation);
         }
-        if (randomSpawn == 4)
+        if (randNumber.numberRandom == 4)
         {
             Instantiate(Character, spawn4, transform.rotation);
         }
+        dogSpawnScript.CharSpawn();
     }
-
 
 }
