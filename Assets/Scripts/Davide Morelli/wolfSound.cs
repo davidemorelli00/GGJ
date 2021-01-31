353fa0bf91143e6e1020d3f996e16990e313adf0
daviidemorelli <davidemorelli@studenti.naba.it>
2021-01-31 04:53:46 +0100

    Tentativo lupo fallito

---
+++ Assets/Scripts/Davide Morelli/wolfSound.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public GameObject Ringhio;
+    public GameObject Attack;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
